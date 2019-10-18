60475e58e61c4f9ddf0dfcc4bed925302b7c4c0c
Valdir Jr <valdirdallagojr@gmail.com>
2019-10-17 22:51:00 -0300

    Conexão com banco e teste de criação no sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello aaaa' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Valdir',
+        email: 'valdir@gmail.com',
+        password_hash: 'dasdwdwa',
+    });
+    return res.json(user);
 });
 
 export default routes;
