81550739ea8c3835584fc7da9a6993b5053966b6
Valdir Jr <valdirdallagojr@gmail.com>
2019-11-22 20:53:09 -0300

    Validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -23,17 +24,38 @@ class AppointmentController {
         const isProvider = await User.findOne({
             where: { id: provider_id, provider: true },
         });
-
         if (!isProvider) {
             return res
                 .status(400)
                 .json({ error: 'provider_id não é um provider' });
         }
 
+        /**
+         * verificar se a data já passou
+         */
+        const hourStart = startOfHour(parseISO(date));
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'data não permitida' });
+        }
+
+        /**
+         * verificar se data está disponivel
+         */
+        const dataNaoDisp = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+        if (dataNaoDisp) {
+            return res.status(400).json({ error: 'data não disponivel' });
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date,
+            date: hourStart,
         });
 
         return res.json(appointment);
